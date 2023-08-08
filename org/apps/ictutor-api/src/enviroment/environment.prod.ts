a59f67438d0dd92a3094b50ef284e2e63aad6786
Quincy van Deursen <q.vandeursen@student.avans.nl>
2023-08-08 16:34:21 +0200

    .

--- org/apps/ictutor-api/src/enviroment/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    JWT_SECRET: 'bFcyJnHGZrRRvHKUjiG5ydro4D5YpF3leZjlp9NHo8RFJEk8q7acFHNjTESZMxG',
-    //TODO: change this to a real mongodb connection string
-    DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/ictutor-db',
-  };
-  
--- org/apps/ictutor-api/src/enviroment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    JWT_SECRET: 'bFcyJnHGZrRRvHKUjiG5ydro4D5YpF3leZjlp9NHo8RFJEk8q7acFHNjTESZMxG',
-    DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/ictutor-db',
-  };
-  
